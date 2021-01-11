d199cee8c708e1a4df9f4e1e480f7011d39bf4e2
Ilya Edelshtein <ilaltim@gmail.com>
2021-01-11 15:51:03 +0200

    added: redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import './App.css';
+import Login from "./components/login";
+import UserContainer from "./components/user";
+import AdminContainer from "./components/admin";
+import {useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div >
 
-    </div>
-  );
+    const appState = useSelector(state => state.app);
+    const {loggedIn,activeId, isAdmin} = appState;
+    console.log(loggedIn, activeId,isAdmin)
+
+    const renderEl = () => {
+
+        if(loggedIn && !isAdmin){
+            return <UserContainer />
+        }
+        if(loggedIn && isAdmin){
+            return <AdminContainer />
+        }
+        return <Login />
+    }
+
+  return (
+    renderEl()
+);
 }
 
 export default App;
