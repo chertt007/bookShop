b9f12dd00296ab263fed584ed99bb60cdbe0d5d0
Ilya Edelshtein <ilaltim@gmail.com>
2021-01-12 12:59:39 +0200

    added: logout functional implemented

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
 } from "react-router-dom";
 import BookContainer from "./components/common/BookContainer";
 import AddBook from "./components/addBook";
+import Header from "./components/header";
+import Logout from "./components/logout";
 
 
 
@@ -19,7 +21,6 @@ import AddBook from "./components/addBook";
     //todo localStorage token
     const appState = useSelector(state => state.app);
     const {loggedIn,activeId, isAdmin,activeName} = appState;
-    console.log(loggedIn, activeId,isAdmin)
 
 
 
@@ -27,24 +28,14 @@ import AddBook from "./components/addBook";
 
 
       <Router>
-          <header>
-              <NavLink to='/'><h2>LOGO</h2></NavLink>
-              {loggedIn ?  <div>
-                  <h3>Your Name : {activeName} </h3>
-                  <h3>Your Role : {isAdmin ? 'Administrator' : 'User'}
-                  </h3>
-              </div> : <h3>HI Guest, please sign in to get access to service</h3>}
-
-           <NavLink to='/login'><button>{loggedIn ? 'log out' : 'sign in'}</button></NavLink>
-          </header>
-
+              <Header isAdmin={isAdmin} loggedIn={loggedIn} activeName={activeName}/>
       <Switch>
           <Route path='/' exact render={()=>{
               if(isAdmin && loggedIn){
                   return <Redirect to='/admin'/>
               }
               if(!isAdmin && loggedIn){
-                  debugger
+
                   return <Redirect to='/user'/>
               }
               return <h2>Public Container</h2>
@@ -54,7 +45,7 @@ import AddBook from "./components/addBook";
                   return <Redirect to='/'/>
               }
               if(!isAdmin && loggedIn){
-                  debugger
+
                   return <Redirect to='/'/>
               }
              return <Login/>
@@ -76,6 +67,9 @@ import AddBook from "./components/addBook";
           }}/>
           <Route path='/add/publisher' exact render={()=>{
               return (<h1>Adding publishers function in development </h1>)
+          }}/>
+          <Route path='/logout' exact render={()=>{
+             return <Logout />
           }}/>
       </Switch>
       </Router>
